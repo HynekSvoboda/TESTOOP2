cb195283e150cfc74f1d9dd5b0c9cbf6f6a808a7
HynekSvoboda <97514770+HynekSvoboda@users.noreply.github.com>
2023-01-26 13:19:18 +0100

    Add files via upload

---
+++ TESTOOP/TESTOOP/HerniPostava.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TESTOOP
+{
+    class HerniPostava
+    {
+        protected string jmeno;
+       protected int poziceX=0;
+       protected int poziceY=0;
+       protected int level = 1;
+        int PoziceX
+        {
+            get
+            {
+               return poziceX;
+            }
+        }
+        int PoziceY
+        {
+            get
+            {
+                return poziceY;
+            }
+        }
+        string Jmeno
+        {
+            get
+            {
+                return jmeno;
+            }
+            set
+            {
+                if (value.Length > 10)MessageBox.Show("Příliš dlouhé jméno!");
+                else jmeno = value;
+            }
+        }
+
+        public HerniPostava(string jmeno)
+        {
+            this.Jmeno = jmeno;
+        }
+        public virtual void ZmenaPozice()
+        {
+            poziceX += 10;
+            poziceY += 10;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}, level {1}, pozice X {2}, pozice Y: {3} ,", Jmeno, level, PoziceX, PoziceY);
+        }
+
+    }
+}
---
+++ TESTOOP/TESTOOP/NPC.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TESTOOP
+{
+    class NPC:HerniPostava
+    {
+
+       public bool sila=false;
+        public int prace = 0;
+        enum Prace
+        {
+            obchodník,
+            nepřítel,
+            obyvatel
+        }
+        public NPC(string jmeno, int prace, bool sila):base(jmeno)
+        {
+            this.prace = prace;
+            this.sila = sila;
+        }
+        public NPC(string jmeno, int prace) : base(jmeno)
+        {
+            sila = false;
+        }
+
+        public override sealed void ZmenaPozice()
+        {
+            poziceX = 50;
+            poziceY = 50;
+        }
+        public override string ToString()
+        {
+            string pracevypis;
+            if (prace == 0) pracevypis = Prace.obchodník.ToString();
+            else if (prace == 1) pracevypis = Prace.nepřítel.ToString();
+            else pracevypis = Prace.obyvatel.ToString();
+            if(sila) return base.ToString() + string.Format(", práce: {0} a je boss",pracevypis);
+            else return base.ToString() + string.Format(" práce: {0} a není boss", pracevypis);
+        }
+    }
+}
